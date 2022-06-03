62fd19861f3ab58901131f208cb8c284f4c34023
alexanderostlingWork <alexander@enestedt.se>
2022-06-03 11:42:10 +0200

    Alexander commit
    Ändrade header, meny och Hero section

--- src/components/hero.js
+++ src/components/hero.js
@@ -12,28 +12,24 @@ import {
   Subhead,
   Text,
 } from "./ui"
+import {
+  startHeroBackground,
+  startHeroContent
+} from "./hero.css"
 
 export default function Hero(props) {
   return (
-    <Section>
+    <Section className={startHeroBackground}>
       <Container>
-        <Flex gap={4} variant="responsive">
-          <Box width="half">
-            {props.image && (
-              <GatsbyImage
-                alt={props.image.alt}
-                image={getImage(props.image.gatsbyImageData)}
-              />
-            )}
-          </Box>
-          <Box width="half">
+        <Flex className={startHeroContent} gap={4} variant="responsive">
+          <Box width="full">
             <Heading as="h1">
               {props.kicker && <Kicker>{props.kicker}</Kicker>}
               {props.h1}
             </Heading>
             <Subhead as="h2">{props.subhead}</Subhead>
             <Text as="p">{props.text}</Text>
-            <ButtonList links={props.links} />
+            
           </Box>
         </Flex>
       </Container>
